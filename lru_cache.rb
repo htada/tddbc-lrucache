cdcc86b75a13d809837482164052a0a2ac07d9a4
mitim <enonon@hotmail.com>
2009-12-30 04:20:09 +0900

    lru_cache_spec.rb のヘルパーメソッドを整理、Mixin化 lru_cache_spec.rb にマルチスレッドテストを追加 lru_cache.rb をマルチスレッド化(Monitorを使用)

--- lru_cache.rb
+++ lru_cache.rb
@@ -1,19 +1,25 @@
+require 'monitor'
+
 class LruCache
+  include MonitorMixin
   attr_reader :limit
 
   def initialize(size, lifespan = 10)
     raise ArgumentError.new unless valid_size?(size)
+	super()
     @limit = size
     @cache = []
     @lifespan = lifespan
   end
 
   def put(key, value)
-    remove_cache(key) if pick_out(key) != nil
-    @cache << CacheValue.new(key, value)
-    if @cache.size > @limit then
-      @cache.shift
-    end
+    synchronize do
+      remove_cache(key) if pick_out(key) != nil
+      @cache << CacheValue.new(key, value)
+      if @cache.size > @limit then
+        @cache.shift
+      end
+	end
   end
 
   def get(key)
@@ -26,15 +32,13 @@ class LruCache
   end
 
   def resize(size)
-    raise ArgumentError.new unless valid_size?(size)
-    (@limit - size).times do
-      @cache.shift
-    end
-    @limit = size
-  end
-
-  def valid_size?(size)
-    return size != nil && size > 0
+    synchronize do
+      raise ArgumentError.new unless valid_size?(size)
+      (@limit - size).times do
+        @cache.shift
+      end
+      @limit = size
+	end
   end
   
   def eldest_key
@@ -46,9 +50,13 @@ class LruCache
     ret = pick_out(key)
     return ret == nil ? nil : ret.birthtime
   end
-
+  
   private
-
+  def valid_size?(size)
+    return size != nil && size > 0
+  end
+  
+  private
   def pick_out(key)
     remove_dead_caches
     @cache.each do |v|
@@ -60,17 +68,20 @@ class LruCache
     return nil
   end
   
+  private
   def rotate(value)
     remove_cache(value.key)
     @cache << value
   end
 
+  private
   def remove_cache(key)
     @cache.each do |v|
       @cache.delete(v) if v.key == key
     end
   end
 
+  private
   def remove_dead_caches
     @cache.each do |v|
       lifetime = Time.now - v.birthtime
--- lru_cache_spec.rb
+++ lru_cache_spec.rb
@@ -1,3 +1,4 @@
+#! ruby -Ku
 # -*- coding: utf-8 -*-
 require 'lru_cache'
 
@@ -23,7 +24,7 @@ describe LruCache do
 
   describe "値の出し入れに関するテスト" do
     before :each do
-      @targ = LruCache.new(3)
+      @targ = create_lru_cache(3)
     end
     
     it "入れたものが同じキーで取りだせること" do
@@ -34,12 +35,12 @@ describe LruCache do
     end
 
     it "キャッシュの中にないキーを取り出すとnilが返ること" do
-      fill(@targ, "a", "b", "c")
+      @targ.fill("a", "b", "c")
       @targ.get("d").should be_nil
     end
 
     it "キャッシュがサイズを越えない場合、キャッシュの中で最も古いキーが取得できること" do
-      fill(@targ, "a", "b", "c")
+      @targ.fill("a", "b", "c")
       @targ.eldest_key.should == "a"
     end
 
@@ -48,19 +49,19 @@ describe LruCache do
     end
 
     it "キャッシュがサイズを越えた場合、越えた分の値が消えていること" do
-      fill(@targ, "a", "b", "c", "d")
+      @targ.fill("a", "b", "c", "d")
       @targ.get("a").should be_nil
       @targ.eldest_key.should == "b"
     end
 
     it "現在キャッシュされている値の個数が取得できること" do
       @targ.size.should == 0
-      fill(@targ, "a", "b")
+      @targ.fill("a", "b")
       @targ.size.should == 2
     end
     
     it "同じキーを渡した場合、上書きされること" do
-      fill(@targ, "a", "b")
+      @targ.fill("a", "b")
       @targ.size.should == 2
       @targ.put("a", "x")
       @targ.size.should == 2
@@ -68,7 +69,7 @@ describe LruCache do
     end
 
     it "最も古いキーをgetすると次に古いキーが最も古いキーとして取得できること" do
-      fill(@targ, "a", "b", "c")
+      @targ.fill("a", "b", "c")
       @targ.eldest_key.should == "a"
       @targ.get("a")
       @targ.eldest_key.should == "b"
@@ -77,8 +78,8 @@ describe LruCache do
 
   describe "キャッシュサイズ変更に関するテスト" do
     before :each do
-      @targ = LruCache.new(3)
-      fill(@targ, "a", "b", "c")
+      @targ = create_lru_cache(3)
+      @targ.fill("a", "b", "c")
     end
 
     it "新しいキャッシュサイズにマイナス値を渡した場合、例外が発生すること" do
@@ -102,14 +103,14 @@ describe LruCache do
     it "キャッシュサイズを増やした場合、キャッシュの内容が変わらないこと" do
       @targ.resize(4)
       @targ.size.should == 3
-      should_have(@targ, "a", "b", "c")
+      @targ.should_have("a", "b", "c")
     end
 
     it "キャッシュサイズを減らした場合、リミットを越えたキャッシュが消えること" do
       @targ.resize(2)
       @targ.size.should == 2
-      should_not_have(@targ, "a")
-      should_have(@targ, "b", "c")
+      @targ.should_not_have("a")
+      @targ.should_have("b", "c")
     end
 
     it "キャッシュが空の場合に、キャッシュサイズを変更してもエラーが起こらないこと" do
@@ -121,9 +122,9 @@ describe LruCache do
   describe "キャッシュの保持期間に関するテスト" do
     before :each do
       # 保存期間に10秒を設定する
-      @targ = LruCache.new(4, 10)
+      @targ = create_lru_cache(4)
       @filled_time = now
-      fill(@targ, "a", "b", "c")
+      @targ.fill("a", "b", "c")
     end
 
     it "キャッシュが登録された時間が取得できること" do
@@ -131,41 +132,117 @@ describe LruCache do
     end
 
     it "保持期間を過ぎたキャッシュが消えること" do
-      should_have(@targ, "a")
+      @targ.should_have("a")
       set_forward(9)
-      should_have(@targ, "a")
+      @targ.should_have("a")
       set_forward(1)
-      should_not_have(@targ, "a")
+      @targ.should_not_have("a")
     end
 
     it "保持期間を過ぎていないキャッシュが消えないこと" do
       set_forward(9)
       @targ.put("d", "D")
       set_forward(1)
-      should_not_have(@targ, "a", "b", "c")
-      should_have(@targ, "d")
+      @targ.should_not_have("a", "b", "c")
+      @targ.should_have("d")
       set_forward(9)
-      should_not_have(@targ, "d")
+      @targ.should_not_have("d")
+    end
+  end
+  
+  describe "スレッドセーフに関するテスト" do
+    before :each do
+      @targ = create_lru_cache(3)
+      @targ.fill("a", "b")
+    end
+	
+    it "順番にスレッドを実行すると、通常通り更新と追加ができること" do
+	  t1 = Thread.start do
+	    @targ.put("a", "X")
+	    @targ.get("a").should == "X"
+	  end
+	  t2 = Thread.start do
+	    @targ.put("c", "Y")
+	    @targ.get("c").should == "Y"
+	  end
+	  t1.join
+	  t2.join
+      @targ.get("a").should == "X"
+	  @targ.get("c").should == "Y"
+	end
+	
+	it "主スレッド内でロック中に別スレッドでputすると、主スレッドがロック解除後にputされること" do
+	  t = nil
+	  @targ.synchronize do
+	    t = Thread.start do
+	      @targ.put("a", "X")
+	      @targ.put("c", "Y")
+	    end
+	    @targ.get("a").should == "a"
+	    @targ.get("c").should be_nil
+	  end
+	  t.join
+      @targ.get("a").should == "X"
+	  @targ.get("c").should == "Y"
+      @targ.eldest_key.should == "b"
+	end
+	
+	it "主スレッド内でロック中に別スレッドでputしつつ主スレッドでもputすると、主スレッドのputが反映され、ロック解除後に別スレッドのputが反映されること" do
+	  t = nil
+	  @targ.synchronize do
+	    t = Thread.start do
+	      @targ.put("a", "X")
+	      @targ.put("c", "Y")
+	    end
+		@targ.put("a", "A")
+		@targ.put("c", "C")
+	    @targ.get("a").should == "A"
+	    @targ.get("c").should == "C"
+	  end
+	  t.join
+      @targ.get("a").should == "X"
+	  @targ.get("c").should == "Y"
+      @targ.eldest_key.should == "b"
+	end
+	
+	it "スレッド内でロック中に別スレッドでresizeすると、主スレッドがロック解除後にresizeが反映されること" do
+	  t = nil
+	  @targ.synchronize do
+	    t = Thread.start do
+		  @targ.resize(2)
+		end
+		@targ.limit.should == 3
+	  end
+	  t.join
+	  @targ.limit.should == 2
+	end
+  end
+end
+
+module TestMethods
+  def fill(*keys)
+    keys.each do |v|
+      put(v, v)
+    end
+  end
+
+  def should_have(*keys)
+    keys.each do |v|
+      get(v).should_not == nil
+    end
+  end
+
+  def should_not_have(*keys)
+    keys.each do |v|
+      get(v).should == nil
     end
   end
 end
 
-def fill(targ, *keys)
-  keys.each do |v|
-    targ.put(v, v)
-  end
-end
-
-def should_have(targ, *keys)
-  keys.each do |v|
-    targ.get(v).should_not be_nil
-  end
-end
-
-def should_not_have(targ, *keys)
-  keys.each do |v|
-    targ.get(v).should be_nil
-  end
+def create_lru_cache(num)
+  targ = LruCache.new(num, 10)
+  targ.extend TestMethods
+  return targ
 end
 
 def now
